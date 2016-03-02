816812ce517e4c81ddb256671c418fd31fed0e05
dcfvg <qwsx@dcfvg.com>
2016-03-02 12:26:30 +0100

    adding basic stack

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var gutil = require( 'gulp-util' );
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+
+gulp.task('less', function() {
+    return gulp.src('./app/assets/less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./app/assets/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+      './bower_components/jquery/dist/jquery.js',
+      './bower_components/lodash/lodash.js',
+      './bower_components/handlebars/handlebars.min.js',
+      './bower_components/bootstrap/dist/js/bootstrap.js'
+      ],
+      {base: 'bower_components/'}
+    )
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./app/assets/js/'));
+});
+
+gulp.task('serve', function() {
+    browserSync.init({ server: "./" });
+
+    gulp.watch('./app/assets/less/*.less', ['less']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('build',['less', 'js']);
+gulp.task('default', ['build', 'serve']);
