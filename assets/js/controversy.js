5be130372b2b87de52e829b3fb27210aea6f06eb
dcfvg <qwsx@dcfvg.com>
2016-03-02 14:17:22 +0100

    layout and template

--- assets/js/controversy.js
+++ assets/js/controversy.js
@@ -1,15 +1,28 @@
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1hnp3GYtdxWdCtOLu_TrwWYlF9iTEgU5Ef4csmo_3i-Y/pubhtml';
 
 $(document).ready( function() {
-  Tabletop.init( { key: public_spreadsheet_url,
-                   callback: showInfo,
-                   parseNumbers: true } );
+  Tabletop.init( { key: public_spreadsheet_url, callback: showInfo, parseNumbers: true } );
 });
 
 function showInfo(data, tabletop) {
 
   var templates = getTemplates();
-  $('#search').html(templates.list( {elements:data.list.elements} ));
+  var elements = data.list.elements;
+
+  $('#results').html(templates.list( {elements:elements} ));
+
+    $("input[type='text']").on("input", function(){
+      var searchStr = $( this ).val();
+      var filtered = _.filter(elements, function (obj) {
+        return _.values(obj).some(function (el) {
+          var t = _.toString(el).toLowerCase();
+          return t.indexOf(searchStr.toLowerCase()) > -1;
+        });
+    });
+
+    $('#results').html(templates.list( {elements:filtered} ));
+
+  });
 }
 
 
